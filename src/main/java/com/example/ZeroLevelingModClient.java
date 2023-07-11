a1523bd49959e0ee838a3a8aa6183338ea40d951
Stepan Ermoshin <esastep20@gmail.com>
2023-07-11 18:56:50 +0300

    Initial commit

---
+++ src/main/java/com/example/ZeroLevelingModClient.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ZeroLevelingModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
