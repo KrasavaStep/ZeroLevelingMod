82960989dde057a592ebbaf085feeb35d4d056b9
Stepan Ermoshin <esastep20@gmail.com>
2023-07-11 20:39:32 +0300

    TASK1: add base for adding custom structure, add test structures

--- src/main/java/com/example/ZeroLevelingMod.java
+++ src/main/java/com/example/ZeroLevelingMod.java
@@ -1,22 +1,20 @@
 package com.example;
 
+import com.example.world.structures.ModStructures;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ZeroLevelingMod implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
+
+	public static final String MOD_ID = "zerolevelingmod";
     public static final Logger LOGGER = LoggerFactory.getLogger("zeroleveling");
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+		LOGGER.info("Hello Zero Leveling!");
 
-		LOGGER.info("Hello Fabric world!");
+		ModStructures.registerStructureFeatures();
 	}
 }
